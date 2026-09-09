3d45bfa16b97d0ed7113870bf3e3eceae812a582
nesrinesaad004-pixel <nesrinesaad004@gmail.com>
2026-09-09 17:48:07 +0100

    Update $domaineId.tsx

--- src/routes/domaines/$domaineId.tsx
+++ src/routes/domaines/$domaineId.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { domainesData } from "../../../programData";
+import { domainesData } from "../../programData";
 import { FileText, Video, ImageIcon, ArrowLeft } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 
